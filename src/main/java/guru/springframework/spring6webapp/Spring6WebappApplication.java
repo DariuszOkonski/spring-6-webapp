fb0ba669233bbb37230be2f81e96c1522debe8db
Dariusz Okoński <darek200180@gmail.com>
2023-11-08 19:31:56 +0100

    add thymeleaf

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -3,6 +3,8 @@ package guru.springframework.spring6webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//http://localhost:8080/h2-console/login.do?jsessionid=bcdc7592ea2dece40199d6663b61ba09
 @SpringBootApplication
 public class Spring6WebappApplication {
 
